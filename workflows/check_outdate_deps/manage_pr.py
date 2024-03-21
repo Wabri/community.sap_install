40bc3f1af8c9e9a5bcc87f0cefe6fb681f2b1c84
Wabri <12409541+Wabri@users.noreply.github.com>
2024-03-21 17:49:00 +0100

    feat(workflows/check_outdate_deps): add commit creation
    This is another experiment taken from https://github.blog/changelog/2021-09-13-a-simpler-api-for-authoring-commits/

--- workflows/check_outdate_deps/manage_pr.py
+++ workflows/check_outdate_deps/manage_pr.py
@@ -3,11 +3,12 @@
 import argparse
 import json
 import os
-
 import requests
+
 
 def get_title(requirement_file):
     return f"Auto-update outdated dependencies in {requirement_file}"
+
 
 def find_replace_in_file(file_path, find_str, replace_str):
     with open(file_path, 'r') as file:
@@ -15,6 +16,67 @@ def find_replace_in_file(file_path, find_str, replace_str):
     content = content.replace(find_str, replace_str)
     with open(file_path, 'w') as file:
         file.write(content)
+
+
+def create_branch_if_not_exists(headers, repo, branch, commit_sha):
+    response = requests.get("https://api.github.com/branches/" + branch)
+    if response.status_code == 404: 
+        refs = {"ref":"refs/heads/" + branch, "sha": commit_sha}
+        headers["Accept"] = f"application/vnd.github+json"
+        response = requests.post(f"https://api.github.com/repos/{repo}/git/refs", headers=headers, data=json.dumps(refs))
+        if response.status_code == 201: 
+            print("The branch is created with success")
+        else:
+            print("Something went wrong with the creation of the branch")
+    else:
+        print("The branch esists")
+
+def create_commit_on_branch_with_changes(headers, repo, branch, file_to_change):
+    url = 'https://api.github.com/graphql'
+    headers['Content-Type'] = 'application/json'
+
+    query = """
+    mutation ($input: CreateCommitOnBranchInput!) {
+      createCommitOnBranch(input: $input) {
+        commit {
+          url
+        }
+      }
+    }
+    """
+
+    variables = {
+        "input": {
+            "branch": {
+                "repositoryNameWithOwner": repo,
+                "branchName": branch
+            },
+            "message": {"headline": "Hello from GraphQL!"},
+            "fileChanges": {
+                "additions": [{
+                    "path": file_to_change,
+                    "contents": "`echo 'Hello, GraphQL! | base64`"
+                }],
+            },
+            "expectedHeadOid": "git rev-parse HEAD"
+        }
+    }
+
+    data = {
+        "query": query,
+        "variables": variables
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        result = response.json()
+        commit_url = result['data']['createCommitOnBranch']['commit']['url']
+        print(commit_url[:56])
+    else:
+        print(f"Error: {response.status_code}")
+
+    # TODO: open pr if doesn't exist
+
 
 if __name__ == '__main__':
     # Arguments parsing
@@ -24,6 +86,7 @@ if __name__ == '__main__':
     parser.add_argument("version", type=str, help='The current version of the package')
     parser.add_argument("latest", type=str, help='The latest version of the package')
     args = parser.parse_args()
+    # TODO: check if arguments are filled, if not exit with errors
 
     # Environment variable
     token = str(os.environ.get("GITHUB_TOKEN"))
@@ -34,24 +97,12 @@ if __name__ == '__main__':
     # Set utility variables
     headers = {"Authorization": f"token {token}"}
     branch = "check_outdated_dependencies"
-
-    # Get the branches
-    response = requests.get("https://api.github.com/branches/" + branch)
-
-    # Check the response
-    if response.status_code == 404: 
-        print("The branch doesn't exist")
-
-        # Create the branch
-        refs = {"ref":"refs/heads/" + branch, "sha": commit_sha}
-        headers["Accept"] = f"application/vnd.github+json"
-        response = requests.post(f"https://api.github.com/repos/{repo}/git/refs", headers=headers, data=json.dumps(refs))
-    else:
-        print("The branch esists")
+    create_branch_if_not_exists(headers, repo, branch, commit_sha)
 
     # Replace the line
     line_current = f"{args.package}=={args.version}"
     line_new = f"{args.package}=={args.latest}"
     find_replace_in_file(args.requirement_file, line_current, line_new)
 
-    # TODO: open pr if doesn't exist
+    create_commit_on_branch_with_changes(headers, repo, branch, "test")
+
