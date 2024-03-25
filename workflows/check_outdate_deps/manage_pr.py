9ae9e1259ef0c1b65ba9dda09157d3cdc8778210
Wabri <12409541+Wabri@users.noreply.github.com>
2024-03-25 15:47:24 +0100

    test

--- workflows/check_outdate_deps/manage_pr.py
+++ workflows/check_outdate_deps/manage_pr.py
@@ -33,6 +33,7 @@ def create_branch_if_not_exists(headers, repo, branch, commit_sha):
 
 def create_commit_on_branch_with_changes(branch, file_to_change):
     commit_message = f"Update {file_to_change}"
+    os.system(f"git config --global --add safe.directory /github/workspace")
     os.system(f"git add {file_to_change}")
     os.system(f"git commit --message=\"{commit_message}\"")
     os.system(f"git push origin {branch}")
