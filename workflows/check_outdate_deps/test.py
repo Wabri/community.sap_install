274222b894485053d588c9d3fdb78f7657b660f4
Wabri <12409541+Wabri@users.noreply.github.com>
2024-03-26 16:35:57 +0100

    test

--- workflows/check_outdate_deps/test.py
+++ workflows/check_outdate_deps/test.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     parser.add_argument("requirement_file", type=str, help='The path to the requirement file')
     args = parser.parse_args()
     current_packages = build_packages_dict_from_file(args.requirement_file)
-    raw_output_outdated = os.system(f"pip3 list --outdated")
+
+    os.system(f"pip3 install -r {args.requirement_file}")
+    raw_output_outdated = os.system("pip3 list --outdated")
 
     print(raw_output_outdated)
