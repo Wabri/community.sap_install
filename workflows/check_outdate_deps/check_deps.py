7ddb6a05573edaec7fa54141585e8de114fa6a92
Wabri <12409541+Wabri@users.noreply.github.com>
2024-03-28 16:20:10 +0100

    test

--- workflows/check_outdate_deps/check_deps.py
+++ workflows/check_outdate_deps/check_deps.py
@@ -6,13 +6,61 @@ import subprocess
 import requests
 import json
 
+TOKEN = str(os.environ.get("GITHUB_TOKEN"))
+REPOSITORY = str(os.environ.get("GITHUB_REPOSITORY"))
+COMMIT_SHA = str(os.environ.get("GITHUB_SHA"))
+REQUIREMENT_FILE = str(os.environ.get("REQUIREMENT_FILE"))
+HEADERS = {
+    "Authorization": f"token {TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+OPEN_PR = os.environ.get("OPEN_PR")
+
+
+def update_branch_with_changes(branch, file_to_change):
+    os.system(f"""
+git config --global --add safe.directory /github/workspace
+git config --global user.email "dependencycheckbot@linuxlab"
+git config --global user.name "DependencyCheckBot"
+git fetch --prune
+git stash push
+git checkout -b {branch} origin/{branch}
+git stash pop
+git add {file_to_change}
+git commit --message=\"Update {file_to_change}\"
+git push
+    """)
+
+
+def find_replace_in_file(file_path, find_str, replace_str):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    content = re.sub(find_str, replace_str, content)
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def create_branch_if_not_exists(branch, commit_sha):
+    response = requests.get(
+        f"https://api.github.com/repos/{REPOSITORY}/branches/{branch}")
+    if response.status_code == 404:
+        refs = {"ref": "refs/heads/" + branch, "sha": commit_sha}
+        response = requests.post(
+            f"https://api.github.com/repos/{REPOSITORY}/git/refs",
+            headers=HEADERS, data=json.dumps(refs))
+        if response.status_code == 201:
+            print(
+                f"INFO: Branch created -> https://github.com/{REPOSITORY}/tree/{branch}")
+        else:
+            print("ERROR: branch not created")
+    else:
+        print("INFO: the branch already esists")
+
 
 def open_issue_for_package(package, current_version, latest_version):
-    repo = os.environ.get("GITHUB_REPOSITORY")
-    requirement_file = str(os.environ.get("REQUIREMENT_FILE"))
-    issue_title = f"Dependency outdated in {requirement_file}: {
+    issue_title = f"Dependency outdated in {REQUIREMENT_FILE}: {
         package}=={current_version} -> {latest_version}"
-    query = f"repo:{repo} type:issue in:title \"{issue_title}\""
+    query = f"repo:{REPOSITORY} type:issue in:title \"{issue_title}\""
     response = requests.get(
         "https://api.github.com/search/issues", params={"q": query})
     data = response.json()
@@ -20,18 +68,16 @@ def open_issue_for_package(package, current_version, latest_version):
         return data['items'][0]['number']
     else:
         issue_description = f"""
-The package {package} is outdated in {requirement_file}.
+The package {package} is outdated in {REQUIREMENT_FILE}.
 
 The latest version is {latest_version}. Please update the package to the latest version.
 
 Check the package [here](https://pypi.org/project/{package}/{latest_version}/) for more information.
         """
-        token = os.environ.get("GITHUB_TOKEN")
         issue = {"title": issue_title, "body": issue_description}
-        headers = {"Authorization": f"token {token}"}
         response = requests.post(
-            f"https://api.github.com/repos/{repo}/issues",
-            headers=headers,
+            f"https://api.github.com/repos/{REPOSITORY}/issues",
+            headers=HEADERS,
             data=json.dumps(issue))
         if response.status_code == 201:
             return response.json()['number']
@@ -45,7 +91,7 @@ def build_packages_dict_from_file(requirement_file):
     print("-------------")
     print("INFO: create dict from file")
     packages = {}
-    with open(requirement_file, 'r') as file:
+    with open(REQUIREMENT_FILE, 'r') as file:
         lines = file.readlines()
         for line in lines:
             regex_pattern = re.compile(
@@ -75,19 +121,19 @@ def build_packages_dict_from_output(output):
 
 
 if __name__ == '__main__':
-    requirement_file = str(os.environ.get("REQUIREMENT_FILE"))
-
-    os.system(f"pip3 install -r {requirement_file}")
+    os.system(f"pip3 install -r {REQUIREMENT_FILE}")
     raw_output_outdated = subprocess.run(
         ['pip3', 'list', '--outdated'],
         stdout=subprocess.PIPE)
-    current_packages = build_packages_dict_from_file(requirement_file)
+    current_packages = build_packages_dict_from_file(REQUIREMENT_FILE)
     latest_packages = build_packages_dict_from_output(
         raw_output_outdated.stdout.decode('utf-8'))
     issues_number = {}
-
     print("-------------")
     print("INFO: Check outdated dependencies")
+    if OPEN_PR == "True":
+        branch = "automated_dependencies_bot"
+        create_branch_if_not_exists(branch, COMMIT_SHA)
     for package in current_packages.keys():
         if package in latest_packages:
             current_version = current_packages[package]
@@ -95,14 +141,19 @@ if __name__ == '__main__':
             issues_number[package] = open_issue_for_package(
                 package, current_version, latest_version)
 
-            if os.environ.get("OPEN_PR") == "True":
-                # TODO: if OPEN_PR env var is set to true than apply the changes
-                #   and open the pr
-                pass
-
+            if OPEN_PR == "True":
+                line_current = f"({package}==*)"
+                line_latest = f"{package}=={latest_version}"
+                find_replace_in_file(REQUIREMENT_FILE,
+                                     line_current,
+                                     line_latest)
             print(f"""
+            ----------------
             package: {package}
             current: {current_version}
             latest: {latest_version}
             issue: {issues_number[package]}
+            ----------------
             """)
+    if OPEN_PR == "True":
+        update_branch_with_changes(branch, REQUIREMENT_FILE)
